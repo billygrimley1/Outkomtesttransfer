5b53e9cfd378a1fac05b5c3b3baa529cf6142827
William Grimley <billygrimley@live.co.uk>
2025-02-07 07:31:35 +0000

    changes to firebase

--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -1,21 +1,28 @@
-// Import the functions you need from the SDKs you need
+// firebase-config.js
+
+// 1. Import the functions you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// If you plan to use Firestore as your database:
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 2. Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCpstrAqMEt8vtK1oUjZQd-ciO8t2bvnYU",
   authDomain: "outkomtest-ba930.firebaseapp.com",
   projectId: "outkomtest-ba930",
-  storageBucket: "outkomtest-ba930.firebasestorage.app",
+  storageBucket: "outkomtest-ba930.firebasestorage.app", // from your console
   messagingSenderId: "356680297231",
   appId: "1:356680297231:web:538f46c8e4e9ae10086c7a",
   measurementId: "G-Y3QX5VW9T5",
 };
 
-// Initialize Firebase
+// 3. Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// 4. (Optional) Export Firestore if you need database features
+export const db = getFirestore(app);
+
+// 5. (Optional) Export analytics if you need it in other parts of your app
+export { analytics };
